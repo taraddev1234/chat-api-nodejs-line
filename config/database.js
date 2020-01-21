8a19c14891fce63ce6d31e8351f78aca4413782c
SaifaN <sayfa@tarad.com>
2020-01-21 23:06:23 +0700

    ##add

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     DB: 'mongodb://localhost:27017/node_rest_api_v2'
+// }
+
+module.exports = { 
+    // DB : 'mongodb+srv://notes:zMWZnGVpBtfLoFWj@cluster0-3z0vk.mongodb.net/Line?retryWrites=true&w=majority'
+    DB : 'mongodb+srv://linechat:7yNppoqtYkQu2jnm@cluster0-3z0vk.mongodb.net/Line?retryWrites=true&w=majority'
+}
