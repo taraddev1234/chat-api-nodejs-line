eaa5f60ab024bb29ad1757b4e8ef314715b4d9ce
SaifaN <sayfa@tarad.com>
2020-01-21 22:20:22 +0700

    ## add code useUnifiedTopology: true in file server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
     // connection to mongodb
     // mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    mongoose.connect(config.DB, { useNewUrlParser: true,useUnifiedTopology: true }).then(
         () => {console.log('Database is connected') },
         err => { console.log('Can not connect to the database'+ err)}
     );
